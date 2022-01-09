22db503bf51c08f398e82e51a6a0516e5ac692e6
Jordan Haynes <Jordancautious@gmail.com>
2022-01-09 00:21:49 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Jordan Haynes on 1/9/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
